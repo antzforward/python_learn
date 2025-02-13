e30ae58853ecc93a088d80813348de76e2cd0a95
刘峰 <liufeng4@kingsoft.com>
2025-02-13 10:18:25 +0800

    -补充信息，在于处于不同decode出错的情况，产生一个递归调用async的方式 return await check_tab_file(filename, encodes[index+1]) 的用法

--- 01Python-Cookbook/Python-Cookbook3rd/05 Files and IO/TabKeyChecker.py
+++ 01Python-Cookbook/Python-Cookbook3rd/05 Files and IO/TabKeyChecker.py
@@ -1,26 +1,28 @@
 import csv
+import time
 from collections import defaultdict
 import pathlib
 import subprocess
 import asyncio
 import sys
 
-async def check_tab_file(filename):
+async def check_tab_file(filename,encode='utf-8'):
     '''
     异步处理单个文件
     :param filename:
     :return
     '''
+    encodes = ['utf-8','utf-16','gb18030','ansi']
+    index = encodes.index( encode )
     try:
         # 读取前四行
-        with open(filename, 'r', encoding='utf-8') as f:
+        with open(filename, 'r', encoding=encodes[index]) as f:
             reader = csv.reader(f, delimiter='\t')
 
             # 读取表头
             headers = [next(reader) for _ in range(4)]
-
             # 获取PKEY位置
-            pkey_columns = [i for i, val in enumerate(headers[1]) if val.startswith('PKEY')]
+            pkey_columns = [i for i, val in enumerate(headers[1]) if val.startswith('PKEY') or val == 'FUnique()']
 
             if not pkey_columns:
                 return False,f"{filename}: 错误：未找到PKEY定义"
@@ -60,11 +62,22 @@ async def check_tab_file(filename):
                         results.append(f"\tKey '{key_str}' 重复出现在行：{', '.join(map(str, lines))}")
                 results.append(f"}}")
             if len(results) != 0:
-                return False,f"{filename}:{'\n'.join(results)}"
+                result_str = "\n".join(results)  # 先创建带换行的结果字符串
+                return False,f"{filename}:{result_str}"
             else:
                 return True,f"{filename}:所有Key值唯一"
+    except UnicodeDecodeError as e:
+        if index + 1 < len(encodes):
+            return await check_tab_file(filename, encodes[index+1])
+        else:
+            return False, str({"status": "error", "filename": filename, "message": f"未知错误: {str(e)}"})
+    except UnicodeError as e:
+        if index + 1 < len(encodes):
+            return await check_tab_file(filename, encodes[index+1])
+        else:
+            return False, str({"status": "error", "filename": filename, "message": f"未知错误: {str(e)}"})
     except Exception as e:
-        return False,str({"status": "error", "filename": filename, "message": f"未知错误: {str(e)}"})
+        return False,str({"status": "error", "filename": filename, "message": f"未知错误: {str(e),type(e)}"})
 
 async def main(dir_name='.'):
     pattern = r'\.tab$'
@@ -78,4 +91,9 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("使用方法：python TabKeyCheck.py .")
         sys.exit(1)
+    start_time = time.time()
     asyncio.run(main(sys.argv[1]))
+    end_time = time.time()
+    # 计算并打印总耗时
+    total_time = end_time - start_time
+    print(f"总体消耗时间: {total_time} 秒")
