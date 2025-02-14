0d2db9bb75bf23a992abfdbfc16101fd74eb07d1
刘峰 <liufeng4@kingsoft.com>
2025-02-14 17:47:45 +0800

    -经过测试 Async 还是协程的写法，在GIL下并不是一个好的实现，嗯，多线程也是个不行的东西。还是考虑在其他语言下面处理这个问题。

--- 01Python-Cookbook/Python-Cookbook3rd/05 Files and IO/TabKeyChecker.py
+++ 01Python-Cookbook/Python-Cookbook3rd/05 Files and IO/TabKeyChecker.py
@@ -6,93 +6,208 @@ import subprocess
 import asyncio
 import sys
 import psutil
+import concurrent
 
-async def check_tab_file(filename,index=0):
+
+def check_tab_file(filename):
     '''
     异步处理单个文件
     :param filename:
     :return
     '''
     encodes = ['utf-8','utf-16','gb18030','ansi']
-    try:
-        # 读取前四行
-        with open(filename, 'r', encoding=encodes[index]) as f:
-            reader = csv.reader(f, delimiter='\t')
+    for index in range(len(encodes)):
+        try:
+            # 读取前四行
+            with open(filename, 'r', encoding=encodes[index]) as f:
+                reader = csv.reader(f, delimiter='\t')
 
-            # 读取表头
-            headers = [next(reader) for _ in range(4)]
-            # 获取PKEY位置
-            pkey_columns = [i for i, val in enumerate(headers[1]) if val.startswith('PKEY') or val == 'FUnique()']
+                # 读取表头
+                headers = [next(reader) for _ in range(4)]
+                # 获取PKEY位置
+                pkey_columns = [i for i, val in enumerate(headers[1]) if val.startswith('PKEY') or val == 'FUnique()']
 
-            if not pkey_columns:
-                return False,f"{filename}: 错误：未找到PKEY定义"
+                if not pkey_columns:
+                    return False,f"{filename}: 错误：未找到PKEY定义"
 
-            # 创建字典记录行号
-            key_dict = defaultdict(list)
-            duplicates_found = False
+                # 创建字典记录行号
+                key_dict = defaultdict(list)
+                duplicates_found = False
 
-            results = []
-            # 从第五行开始检查
-            for line_num, row in enumerate(reader, start=5):
-                if len(row) < max(pkey_columns) + 1:
-                    results.append(f"行 {line_num} 列数不足")
-                    continue
+                results = []
+                # 从第五行开始检查
+                for line_num, row in enumerate(reader, start=5):
+                    if len(row) < max(pkey_columns) + 1:
+                        results.append(f"行 {line_num} 列数不足")
+                        continue
 
-                # 生成组合Key
-                key = tuple(row[i] for i in pkey_columns)
+                    # 生成组合Key
+                    key = tuple(row[i] for i in pkey_columns)
 
-                # 检查Key有效性
-                if any(not k.strip() for k in key):
-                    results.append(f"行 {line_num} 包含空Key值")
-                    continue
+                    # 检查Key有效性
+                    if any(not k.strip() for k in key):
+                        results.append(f"行 {line_num} 包含空Key值")
+                        continue
 
-                # 记录重复情况
-                if key in key_dict:
-                    duplicates_found = True
-                    key_dict[key].append(line_num)
+                    # 记录重复情况
+                    if key in key_dict:
+                        duplicates_found = True
+                        key_dict[key].append(line_num)
+                    else:
+                        key_dict[key] = [line_num]
+
+                # 输出重复结果
+                if duplicates_found:
+                    results.append(f"发现重复Key：{{")
+                    for key, lines in key_dict.items():
+                        if len(lines) > 1:
+                            key_str = ' + '.join(key)
+                            results.append(f"\tKey '{key_str}' 重复出现在行：{', '.join(map(str, lines))}")
+                    results.append(f"}}")
+                if len(results) != 0:
+                    result_str = "\n".join(results)  # 先创建带换行的结果字符串
+                    return False,f"{filename}:{result_str}"
                 else:
-                    key_dict[key] = [line_num]
-
-            # 输出重复结果
-            if duplicates_found:
-                results.append(f"发现重复Key：{{")
-                for key, lines in key_dict.items():
-                    if len(lines) > 1:
-                        key_str = ' + '.join(key)
-                        results.append(f"\tKey '{key_str}' 重复出现在行：{', '.join(map(str, lines))}")
-                results.append(f"}}")
-            if len(results) != 0:
-                result_str = "\n".join(results)  # 先创建带换行的结果字符串
-                return False,f"{filename}:{result_str}"
-            else:
-                return True,f"{filename}:所有Key值唯一"
-    except UnicodeError as e: #UnicodeError or UnicodeEncodeError as e: 可以用or哦，但是他们有继承关系，选父类型即可
-        if index + 1 < len(encodes):
-            return await check_tab_file(filename, index+1)
-        else:
-            return False, str({"status": "error", "filename": filename, "message": f"未知错误: {str(e)}"})
-    except Exception as e:
-        return False,str({"status": "error", "filename": filename, "message": f"未知错误: {str(e),type(e)}"})
-
-async def main(dir_name='.'):
-    pattern = r'\.tab$'
-    out_text = subprocess.check_output(['fd','-a','-t','f',pattern,dir_name]).decode('utf-8')
-    file_list = ( pathlib.Path(f) for f in out_text.split('\n') if f !='' )
-    tasks = [check_tab_file(filename) for filename in file_list]
-    results = await  asyncio.gather( * tasks )
-    for result,log in results:
-        print( log )
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("使用方法：python TabKeyCheck.py tab_folder_path")
-        sys.exit(1)
+                    return True,f"{filename}:所有Key值唯一"
+        except UnicodeError as e: #UnicodeError or UnicodeEncodeError as e: 可以用or哦，但是他们有继承关系，选父类型即可
+            if index + 1 >= len(encodes):
+                return False, str({"status": "error", "filename": filename, "message": f"未知错误: {str(e)}"})
+        except Exception as e:
+            return False,str({"status": "error", "filename": filename, "message": f"未知错误: {str(e),type(e)}"})
+async  def check_tab_file_async(filename):
+    return check_tab_file(filename)
+def main_task_seq(dir_name='.'):
     process = psutil.Process()
     # 获取内存信息
     mem_info = process.memory_info()
     start_time = time.time()
-    asyncio.run(main(sys.argv[1]))
+    pattern = r'\.tab$'
+    out_text = subprocess.check_output(['fd', '-a', '-t', 'f', pattern, dir_name]).decode('utf-8')
+    file_list = (pathlib.Path(f) for f in out_text.split('\n') if f != '')
+    tasks = [check_tab_file(filename) for filename in file_list]
+    # for result,log in results:
+    #    print( log )
     end_time = time.time()
     # 计算并打印总耗时
     total_time = end_time - start_time
-    print(f"总体消耗时间: {total_time} 秒")
-    print(f"使用了内存: {(process.memory_info().rss - mem_info.rss) / 1024 ** 2:.2f} MB")  # RSS为常驻集大小
+    # print(f"总体消耗时间: {total_time} 秒")
+    # print(f"使用了内存: {(process.memory_info().rss - mem_info.rss) / 1024 ** 2:.2f} MB")  # RSS为常驻集大小
+    return f"总体消耗时间: {total_time} 秒", f"使用了内存: {(process.memory_info().rss - mem_info.rss) / 1024 ** 2:.2f} MB"
+async def main_task(dir_name='.'):
+    process = psutil.Process()
+    # 获取内存信息
+    mem_info = process.memory_info()
+    start_time = time.time()
+    pattern = r'\.tab$'
+    out_text = subprocess.check_output(['fd','-a','-t','f',pattern,dir_name]).decode('utf-8')
+    file_list = ( pathlib.Path(f) for f in out_text.split('\n') if f !='' )
+    tasks = [check_tab_file_async(filename) for filename in file_list]
+    results = await asyncio.gather( * tasks )
+    #for result,log in results:
+    #    print( log )
+    end_time = time.time()
+    # 计算并打印总耗时
+    total_time = end_time - start_time
+    #print(f"总体消耗时间: {total_time} 秒")
+    #print(f"使用了内存: {(process.memory_info().rss - mem_info.rss) / 1024 ** 2:.2f} MB")  # RSS为常驻集大小
+    return f"总体消耗时间: {total_time} 秒", f"使用了内存: {(process.memory_info().rss - mem_info.rss) / 1024 ** 2:.2f} MB"
+
+def check_tab_file_thread(filename):
+    '''
+    异步处理单个文件
+    :param filename:
+    :return
+    '''
+    encodes = ['utf-8','utf-16','gb18030','ansi']
+    for index in range(len(encodes)):
+        try:
+            # 读取前四行
+            with open(filename, 'r', encoding=encodes[index]) as f:
+                reader = csv.reader(f, delimiter='\t')
+
+                # 读取表头
+                headers = [next(reader) for _ in range(4)]
+                # 获取PKEY位置
+                pkey_columns = [i for i, val in enumerate(headers[1]) if val.startswith('PKEY') or val == 'FUnique()']
+
+                if not pkey_columns:
+                    return False,f"{filename}: 错误：未找到PKEY定义"
+
+                # 创建字典记录行号
+                key_dict = defaultdict(list)
+                duplicates_found = False
+
+                results = []
+                # 从第五行开始检查
+                for line_num, row in enumerate(reader, start=5):
+                    if len(row) < max(pkey_columns) + 1:
+                        results.append(f"行 {line_num} 列数不足")
+                        continue
+
+                    # 生成组合Key
+                    key = tuple(row[i] for i in pkey_columns)
+
+                    # 检查Key有效性
+                    if any(not k.strip() for k in key):
+                        results.append(f"行 {line_num} 包含空Key值")
+                        continue
+
+                    # 记录重复情况
+                    if key in key_dict:
+                        duplicates_found = True
+                        key_dict[key].append(line_num)
+                    else:
+                        key_dict[key] = [line_num]
+
+                # 输出重复结果
+                if duplicates_found:
+                    results.append(f"发现重复Key：{{")
+                    for key, lines in key_dict.items():
+                        if len(lines) > 1:
+                            key_str = ' + '.join(key)
+                            results.append(f"\tKey '{key_str}' 重复出现在行：{', '.join(map(str, lines))}")
+                    results.append(f"}}")
+                if len(results) != 0:
+                    result_str = "\n".join(results)  # 先创建带换行的结果字符串
+                    return False,f"{filename}:{result_str}"
+                else:
+                    return True,f"{filename}:所有Key值唯一"
+        except UnicodeError as e: #UnicodeError or UnicodeEncodeError as e: 可以用or哦，但是他们有继承关系，选父类型即可
+            if index + 1 >= len(encodes):
+                return False, str({"status": "error", "filename": filename, "message": f"未知错误: {str(e)}"})
+        except Exception as e:
+            return False,str({"status": "error", "filename": filename, "message": f"未知错误: {str(e),type(e)}"})
+
+async def main_thread(dir_name='.'):
+    process = psutil.Process()
+    # 获取内存信息
+    mem_info = process.memory_info()
+    start_time = time.time()
+    pattern = r'\.tab$'
+    out_text = subprocess.check_output(['fd','-a','-t','f',pattern,dir_name]).decode('utf-8')
+    file_list = ( pathlib.Path(f) for f in out_text.split('\n') if f !='' )
+    async def check_tab_file( filename ):
+        loop = asyncio.get_event_loop()
+        with concurrent.futures.ThreadPoolExecutor() as pool:
+            # 将同步函数提交到线程池
+            return await loop.run_in_executor(pool, check_tab_file_thread, filename)
+
+    tasks = [check_tab_file(f) for f in file_list]
+    results = await asyncio.gather(*tasks)
+    #for result,log in results:
+    #    print( log )
+    end_time = time.time()
+    # 计算并打印总耗时
+    total_time = end_time - start_time
+    #print(f"总体消耗时间: {total_time} 秒")
+    #print(f"使用了内存: {(process.memory_info().rss - mem_info.rss) / 1024 ** 2:.2f} MB")  # RSS为常驻集大小
+    return f"总体消耗时间: {total_time} 秒",f"使用了内存: {(process.memory_info().rss - mem_info.rss) / 1024 ** 2:.2f} MB"
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("使用方法：python TabKeyCheck.py tab_folder_path")
+        sys.exit(1)
+    folderpath = sys.argv[1]
+    print(main_task_seq(folderpath))
+    print(asyncio.run(main_task(folderpath)))
+    print(asyncio.run(main_thread(folderpath)))
